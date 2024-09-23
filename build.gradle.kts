b11a11a6ca02576e080532292b6ef542dd91d82a
baka4n <qq474899581@gmail.com>
2024-09-23 19:32:50 +0800

    待补全

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
+import cn.hutool.core.date.LocalDateTimeUtil
 import cn.hutool.core.io.FileUtil
+import cn.hutool.http.HttpUtil
+import cn.hutool.json.JSONArray
 import cn.hutool.json.JSONObject
 import cn.hutool.json.JSONUtil
 import cn.hutool.setting.Setting
+import com.fasterxml.jackson.dataformat.toml.TomlMapper
+
 
 plugins {
-    id("com.tddworks.central-portal-publisher") version "0.0.5"
     id("com.github.hierynomus.license") version "0.15.0"
+    id("org.jreleaser") version "1.+"
+    id("org.kordamp.gradle.java-project") version "0.+"
     base
     signing
 }
@@ -17,9 +23,7 @@ val projDescription: String by rootProject
 group = mavenGroup
 version = projectVersion
 description = projDescription
-base {
-    archivesName = projName
-}
+
 buildscript {
     repositories {
         mavenCentral()
@@ -29,67 +33,100 @@ buildscript {
         classpath("cn.hutool:hutool-json:5.8.31")
 
         classpath("cn.hutool:hutool-setting:5.8.31")
+        classpath("cn.hutool:hutool-http:5.8.31")
     }
 }
+
+var s = Setting(file(".git/config").absolutePath)
+var replace = FileUtil.readUtf8String(file(".git/HEAD")).replace("ref: refs/heads/", "")
+val url = s.get("remote \"origin\"", "url")
+val repo = url.replace(".git", "").replace("https://github.com/", "https://api.github.com/repos/");
+val repos =
+    JSONUtil.parseObj(HttpUtil.get(repo, Charsets.UTF_8))
+var parse =
+    LocalDateTimeUtil.parse(repos.getStr("created_at").replace("Z", "+0000"), "yyyy-MM-dd'T'HH:mm:ssZ")
+
+
+
+var file = file("maven.toml")
+if (!file.exists()) {
+    FileUtil.copyFile(file("gradle/template.toml"), file)
+}
+var mavenToml: JSONObject = JSONUtil.createObj()
+file.bufferedReader(Charsets.UTF_8).use {
+    val readTree = TomlMapper().readTree(it)
+    mavenToml = JSONUtil.parseObj(readTree.toPrettyString())
+}
+
+
 
 allprojects {
     repositories {
         mavenCentral()
         mavenLocal()
+        maven {
+            url = uri("https://central.sonatype.com/api/v1/publisher/deployments/download/")
 
+        }
+    }
+
+    base {
+        archivesName = project.name
+    }
+
+    config {
+        info {
+            name = project.base.archivesName.get()
+            description = project.description
+            inceptionYear = parse.year.toString()
+            vendor = repos.getJSONObject("owner").getStr("login")
+
+            links {
+                website = repos.getStr("html_url")
+                issueTracker = "${repos.getStr("html_url")}/issues"
+                scm = repos.getStr("clone_url")
+            }
+
+            scm {
+                url = repos.getStr("html_url")
+                connection = "scm:git:${repos.getStr("clone_url")}"
+                developerConnection = "scm:git:${repos.getStr("ssh_url")}"
+            }
+
+            val authors: JSONArray = mavenToml.getJSONArray("authors")
+            people {
+                for (any in authors) {
+                    any as JSONObject
+                    person {
+                        id = any.getStr("id")
+                        name = any.getStr("name")
+                        any.getStr("email")?.let {
+                            email = it
+                        }
+                        any.getStr("url")?.let {
+                            url = it
+                        }
+                        any.getJSONObject("organization")?.let { org ->
+                            organization {
+                                name = org.getStr("name")
+                                url = org.getStr("url")
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        }
     }
 }
+
+
 
 subprojects {
     apply(plugin = "maven-publish")
     apply(plugin = "signing")
+    apply(plugin = "org.kordamp.gradle.java-project")
+    apply(plugin = "org.jreleaser")
 }
 
-license {
-    headerURI = file("LICENSE").toURI()
-}
-
-var mavenFile = file("center-maven.json")
-var createObj: JSONObject;
-if (!mavenFile.exists()) {
-    createObj = JSONUtil.createObj()
-    createObj.putOnce("user_name", "")
-    createObj.putOnce("password", "")
-    createObj.putOnce("signing.keyId", "")
-    createObj.putOnce("signing.password", "")
-    mavenFile.createNewFile()
-    mavenFile.bufferedWriter(Charsets.UTF_8).use {
-        it.write(createObj.toStringPretty())
-    }
-} else {
-    mavenFile.bufferedReader(Charsets.UTF_8).use {
-        createObj = JSONUtil.parseObj(it.readText())
-    }
-}
-
-var s = Setting(file(".git/config").absolutePath)
-FileUtil.readUtf8String(file(".git/HEAD")).replace("ref: refs/heads/", "")
-println()
-
-allprojects {
-    setProperty("signing.keyId", createObj.getStr("signing.keyId"))
-    setProperty("signing.password", createObj.getStr("signing.password"))
-    setProperty("POM_NAME", projName)
-    setProperty("POM_DESCRIPTION", projDescription)
-    val url = s.get("remote \"origin\"", "url")
-    setProperty("POM_URL", url.replace(".git", ""))
-    setProperty("POM_SCM_URL", url.replace(".git", ""))
-    setProperty("POM_SCM_CONNECTION", url)
-    setProperty("POM_SCM_DEV_CONNECTION", url)
-}
-
-sonatypePortalPublisher {
-    authentication {
-        username = createObj.getStr("user_name")
-        password = createObj.getStr("password")
-    }
-    settings {
-        autoPublish = false
-        aggregation = true
-    }
-}
