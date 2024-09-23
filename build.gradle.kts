136c4ffce93b1662b79f8cebf3a55c44e4eb9587
baka4n <qq474899581@gmail.com>
2024-09-23 20:58:14 +0800

    自动传输maven仓库完成

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import cn.hutool.core.date.LocalDateTimeUtil
 import cn.hutool.core.io.FileUtil
 import cn.hutool.http.HttpUtil
-import cn.hutool.json.JSONArray
 import cn.hutool.json.JSONObject
 import cn.hutool.json.JSONUtil
 import cn.hutool.setting.Setting
 import com.fasterxml.jackson.dataformat.toml.TomlMapper
+import com.vanniktech.maven.publish.SonatypeHost
 
 
 plugins {
+    `java-library`
     id("com.github.hierynomus.license") version "0.15.0"
-    id("org.jreleaser") version "1.+"
-    id("org.kordamp.gradle.java-project") version "0.+"
+    id("com.vanniktech.maven.publish") version "0.29.0"
     base
     signing
 }
@@ -24,6 +24,10 @@ group = mavenGroup
 version = projectVersion
 description = projDescription
 
+base {
+    archivesName = name
+}
+
 buildscript {
     repositories {
         mavenCentral()
@@ -31,27 +35,30 @@ buildscript {
     }
     dependencies {
         classpath("cn.hutool:hutool-json:5.8.31")
-
+        classpath("com.fasterxml.jackson.dataformat:jackson-dataformat-toml:2.17.2")
         classpath("cn.hutool:hutool-setting:5.8.31")
         classpath("cn.hutool:hutool-http:5.8.31")
     }
 }
 
 var s = Setting(file(".git/config").absolutePath)
-var replace = FileUtil.readUtf8String(file(".git/HEAD")).replace("ref: refs/heads/", "")
-val url = s.get("remote \"origin\"", "url")
-val repo = url.replace(".git", "").replace("https://github.com/", "https://api.github.com/repos/");
+var branch = FileUtil.readUtf8String(file(".git/HEAD")).replace("ref: refs/heads/", "").trim()
+
 val repos =
-    JSONUtil.parseObj(HttpUtil.get(repo, Charsets.UTF_8))
+    JSONUtil.parseObj(HttpUtil.get(s.get("remote \"origin\"", "url")
+            .replace(".git", "")
+            .replace("https://github.com/", "https://api.github.com/repos/"), Charsets.UTF_8))
 var parse =
     LocalDateTimeUtil.parse(repos.getStr("created_at").replace("Z", "+0000"), "yyyy-MM-dd'T'HH:mm:ssZ")
 
 
 
-var file = file("maven.toml")
-if (!file.exists()) {
-    FileUtil.copyFile(file("gradle/template.toml"), file)
+var file = file("maven.toml").apply {
+    if (!exists()) {
+        FileUtil.copyFile(file("gradle/template.toml"), this)
+    }
 }
+
 var mavenToml: JSONObject = JSONUtil.createObj()
 file.bufferedReader(Charsets.UTF_8).use {
     val readTree = TomlMapper().readTree(it)
@@ -70,52 +77,34 @@ allprojects {
         }
     }
 
-    base {
-        archivesName = project.name
-    }
-
-    config {
-        info {
-            name = project.base.archivesName.get()
+    mavenPublishing {
+        publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
+        coordinates(project.group.toString(), base.archivesName.get(), project.version.toString())
+        pom {
+            name = base.archivesName.get()
             description = project.description
             inceptionYear = parse.year.toString()
-            vendor = repos.getJSONObject("owner").getStr("login")
-
-            links {
-                website = repos.getStr("html_url")
-                issueTracker = "${repos.getStr("html_url")}/issues"
-                scm = repos.getStr("clone_url")
+            licenses {
+                license {
+                    name = mavenToml.getStr("license")
+                    url = "${repos.getStr("svn_url")}/blob/$branch/LICENSE"
+                    description = "${repos.getStr("svn_url")}/blob/$branch/LICENSE"
+                }
             }
-
+            developers {
+                mavenToml.getJSONArray("authors").forEach {
+                    it as JSONObject
+                    developer {
+                        id = it.getStr("id")
+                        name = it.getStr("name")
+                        url = it.getStr("url")
+                    }
+                }
+            }
             scm {
                 url = repos.getStr("html_url")
-                connection = "scm:git:${repos.getStr("clone_url")}"
-                developerConnection = "scm:git:${repos.getStr("ssh_url")}"
-            }
-
-            val authors: JSONArray = mavenToml.getJSONArray("authors")
-            people {
-                for (any in authors) {
-                    any as JSONObject
-                    person {
-                        id = any.getStr("id")
-                        name = any.getStr("name")
-                        any.getStr("email")?.let {
-                            email = it
-                        }
-                        any.getStr("url")?.let {
-                            url = it
-                        }
-                        any.getJSONObject("organization")?.let { org ->
-                            organization {
-                                name = org.getStr("name")
-                                url = org.getStr("url")
-                            }
-                        }
-
-                    }
-
-                }
+                connection = "scm:git:${repos.getStr("git_url")}"
+                developerConnection = "scm:git:ssh://${repos.getStr("ssh_url")}"
             }
         }
     }
@@ -125,8 +114,11 @@ allprojects {
 
 subprojects {
     apply(plugin = "maven-publish")
-    apply(plugin = "signing")
-    apply(plugin = "org.kordamp.gradle.java-project")
-    apply(plugin = "org.jreleaser")
+    apply(plugin = "base")
+    apply(plugin = "com.vanniktech.maven.publish")
+
+    base {
+        archivesName = "${rootProject.base.archivesName.get()}-$name"
+    }
 }
 
