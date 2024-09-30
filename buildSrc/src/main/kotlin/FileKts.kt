3604949494e11f0e37a746c4d35958d37360b4fb
baka4n <qq474899581@gmail.com>
2024-09-30 21:52:50 +0800

    改制完成

--- buildSrc/src/main/kotlin/FileKts.kt
+++ buildSrc/src/main/kotlin/FileKts.kt
@@ -1,11 +1,26 @@
 import cn.hutool.core.io.FileUtil
+import cn.hutool.core.io.file.PathUtil
+
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardCopyOption
 
 fun File.copy(file: File) : File {
+
     apply {
         if (!exists()) {
             FileUtil.copyFile(file, this)
         }
+    }
+    return this
+}
+
+fun Path.copy(path: Path) : Path {
+
+    apply {
+
+
     }
     return this
 }
